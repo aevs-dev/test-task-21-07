a584667deef4b049892798c63064a5fc74ab8123
Анатолий <tolyaaaan1208@gmail.com>
2025-07-21 16:28:29 +0400

    * commit

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -25,10 +25,4 @@ class AuthController extends Controller
         return response()->json($response);
     }
 
-    public function logout(): JsonResponse
-    {
-        auth()->logout();
-        return response()->json(['message' => 'Successfully logged out']);
-    }
-
 }
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::post('login', 'login')->withoutMiddleware('auth:api');
-        Route::post('logout', 'logout');
     });
 
     Route::controller(UserController::class)->group(function () {
